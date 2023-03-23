61c8c475d0a441c8a4b1a110cbac5cc997fed1da
AnnaGolitsyna <110576gav@gmail.com>
2023-03-23 13:48:05 +0200

    task1

---
+++ les1-context/context/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ThemedButton from './ThemedButton';
+
+
+const App = () => {
+  return (
+    <div id="root">
+      <div>
+        <ThemedButton />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ les1-context/context/src/ThemedButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ThemedButton = () => {
+  return (
+    <>
+      <button className="btn">Dynamic Theme</button>
+      <button className="btn">Default Theme</button>
+    </>
+  );
+};
+
+export default ThemedButton;
