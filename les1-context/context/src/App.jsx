7398f512a5c11f00c2f34efa609c3e6a116d9e34
AnnaGolitsyna <110576gav@gmail.com>
2023-03-26 17:20:15 +0300

    secBtn

--- les1-context/context/src/App.jsx
+++ les1-context/context/src/App.jsx
@@ -2,52 +2,52 @@ import React, { useState } from 'react';
 import ThemedButton from './ThemedButton';
 import { ThemeContext, themes } from './themes-context';
 
-// class App extends React.Component {
-//   state = {
-//     theme: themes.dark,
-//   };
-
-//   toggleTheme = () => {
-//     const newTheme =
-//       this.state.theme === themes.dark ? themes.light : themes.dark;
-//     this.setState({
-//       theme: newTheme,
-//     });
-
-//   };
-//   render() {
-//     return (
-//       <div id="root">
-//         <div>
-//           <ThemeContext.Provider value={this.state.theme}>
-//             <ThemedButton onClick={this.toggleTheme} text="Dynamic Theme" />
-//           </ThemeContext.Provider>
-//             <ThemedButton text="Default Theme" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-const App = () => {
-  const [theme, setTheme] = useState(themes.dark);
-
-  const toggleTheme = () => {
-    theme === themes.dark ? setTheme(themes.light) : setTheme(themes.dark);
+class App extends React.Component {
+  state = {
+    theme: themes.dark,
   };
 
-  console.log('render');
+  toggleTheme = () => {
+    const newTheme =
+      this.state.theme === themes.dark ? themes.light : themes.dark;
+    this.setState({
+      theme: newTheme,
+    });
 
-  return (
-    <div id="root">
-      <div>
-        <ThemeContext.Provider value={theme}>
-          <ThemedButton text="Dynamic Theme" onClick={toggleTheme} />
-        </ThemeContext.Provider>
-        <ThemedButton text="Default Theme" />
+  };
+  render() {
+    return (
+      <div id="root">
+        <div>
+          <ThemeContext.Provider value={this.state.theme}>
+            <ThemedButton onClick={this.toggleTheme} text="Dynamic Theme" />
+          </ThemeContext.Provider>
+          <ThemedButton onClick={this.toggleTheme} text="Default Theme" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// const App = () => {
+//   const [theme, setTheme] = useState(themes.dark);
+
+//   const toggleTheme = () => {
+//     theme === themes.dark ? setTheme(themes.light) : setTheme(themes.dark);
+//   };
+
+//   console.log('render');
+
+//   return (
+//     <div id="root">
+//       <div>
+//         <ThemeContext.Provider value={theme}>
+//           <ThemedButton text="Dynamic Theme" onClick={toggleTheme} />
+//         </ThemeContext.Provider>
+//         <ThemedButton text="Default Theme" onClick={toggleTheme} />
+//       </div>
+//     </div>
+//   );
+// };
 
 export default App;
