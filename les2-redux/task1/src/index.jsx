db1edc715c979c13255d8917389852e31af9fc72
AnnaGolitsyna <110576gav@gmail.com>
2023-03-30 13:54:24 +0300

    toolkit

--- les2-redux/task1/src/index.jsx
+++ les2-redux/task1/src/index.jsx
@@ -1,32 +1,62 @@
-import { createStore } from 'redux';
+// VAR 2
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+
+export const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
+console.log(store.getState());
+
+
+// VAR 1
+
+// import { createStore } from 'redux';
 
 // const INCREMENT = 'COUNTER/INCREMENT';
 // const DECREMENT = 'COUNTER/DECREMENT';
 
-export const increment = () => {
-  return {
-    type: 'COUNTER/INCREMENT',
-  };
-};
+// export const increment = () => {
+//   return {
+//     type: INCREMENT,
+//   };
+// };
 
-export const decrement = () => {
-  return {
-    type: 'COUNTER/DECREMENT',
-  };
-};
+// export const decrement = () => {
+//   return {
+//     type: DECREMENT,
+//   };
+// };
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'COUNTER/INCREMENT':
-      return state + 1;
-    case 'COUNTER/DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-};
+// const counterReducer = (state = 0, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return state + 1;
+//     case DECREMENT:
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// };
 
-export const store = createStore(counterReducer);
+// export const store = createStore(counterReducer);
 
 // store.dispatch(increment());
 // store.dispatch(increment());
