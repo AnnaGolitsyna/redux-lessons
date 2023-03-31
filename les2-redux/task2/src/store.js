9055d467854a9f5236312782562901d77f39e080
AnnaGolitsyna <110576gav@gmail.com>
2023-03-31 15:16:14 +0300

    toolkit

--- les2-redux/task2/src/store.js
+++ les2-redux/task2/src/store.js
@@ -1,55 +1,84 @@
-// VAR 1
+// VAR 2
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-import { createStore } from 'redux';
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { history: [] },
+  reducers: {
+    increment: (state) => ({
+      ...state,
+      history: [...state.history, 1],
+    }),
+    decrement: (state) => ({
+      ...state,
+      history: [...state.history, -1],
+    }),
+    reset: (state) => ({
+      ...state,
+      history: [],
+    }),
+  },
+});
 
-const INCREMENT = 'COUNTER/INCREMENT';
-const DECREMENT = 'COUNTER/DECREMENT';
-const RESET = 'COUNTER/RESET';
+export const { increment, decrement, reset } = counterSlice.actions;
 
-export const increment = () => {
-  return {
-    type: INCREMENT,
-  };
-};
-
-export const decrement = () => {
-  return {
-    type: DECREMENT,
-  };
-};
-
-export const reset = () => {
-  return {
-    type: RESET,
-  };
-};
-
-const initialState = {
-  history: [],
-};
-
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        history: state.history.concat(1),
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        history: state.history.concat(-1),
-      };
-    case RESET:
-      return {
-        ...state,
-        history: [],
-      };
-    default:
-      return state;
-  }
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
 export default store;
+// VAR 1
+
+// import { createStore } from 'redux';
+
+// const INCREMENT = 'COUNTER/INCREMENT';
+// const DECREMENT = 'COUNTER/DECREMENT';
+// const RESET = 'COUNTER/RESET';
+
+// export const increment = () => {
+//   return {
+//     type: INCREMENT,
+//   };
+// };
+
+// export const decrement = () => {
+//   return {
+//     type: DECREMENT,
+//   };
+// };
+
+// export const reset = () => {
+//   return {
+//     type: RESET,
+//   };
+// };
+
+// const initialState = {
+//   history: [],
+// };
+
+// const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return {
+//         ...state,
+//         history: state.history.concat(1),
+//       };
+//     case DECREMENT:
+//       return {
+//         ...state,
+//         history: state.history.concat(-1),
+//       };
+//     case RESET:
+//       return {
+//         ...state,
+//         history: [],
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// const store = createStore(counterReducer);
+
+// export default store;
