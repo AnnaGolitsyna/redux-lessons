9f1f4d961b4c4bc8ea2fbde4dae9ac73d1b914df
AnnaGolitsyna <110576gav@gmail.com>
2023-04-05 15:42:22 +0300

    DT

--- les4-react-redux/task1/src/store.js
+++ les4-react-redux/task1/src/store.js
@@ -1,10 +1,6 @@
 import { createStore } from 'redux';
 import counterReducer from './counter.reducer';
 
-
-const store = createStore(
-  counterReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(counterReducer);
 
 export default store;
