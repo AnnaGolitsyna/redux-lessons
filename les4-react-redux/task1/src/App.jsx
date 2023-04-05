f720d2499b43f930a24a450524d1000db0cb6d47
AnnaGolitsyna <110576gav@gmail.com>
2023-04-05 15:33:03 +0300

    structure

--- les4-react-redux/task1/src/App.jsx
+++ les4-react-redux/task1/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import Counter from "./Counter";
+import Counter from "./components/Counter.jsx";
 import store from './store.js';
 
 const App = () => {
