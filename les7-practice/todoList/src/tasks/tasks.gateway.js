2589bf5bffd12b02cb0fa35a47adb9236ce69222
AnnaGolitsyna <110576gav@gmail.com>
2023-04-19 15:23:35 +0300

    asyncAwait

--- les7-practice/todoList/src/tasks/tasks.gateway.js
+++ les7-practice/todoList/src/tasks/tasks.gateway.js
@@ -1,46 +1,60 @@
 const baseUrl = 'https://639acdd831877e43d676cd31.mockapi.io/logins';
 
-export const fetchCreateTask = (taskData) => {
-  return fetch(baseUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(taskData),
-  }).then((response) => {
+export const fetchCreateTask = async (taskData) => {
+  try {
+    const response = await fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(taskData),
+    });
     if (!response.ok) {
       throw new Error('Failed to create task');
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const fetchUpdateTask = (taskData, taskId) => {
-  return fetch(`${baseUrl}/${taskId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(taskData),
-  }).then((response) => {
+export const fetchUpdateTask = async (taskData, taskId) => {
+  try {
+    const response = await fetch(`${baseUrl}/${taskId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(taskData),
+    });
     if (!response.ok) {
       throw new Error('Failed to update task');
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const fetchDeleteTask = (taskId) => {
-  return fetch(`${baseUrl}/${taskId}`, {
-    method: 'DELETE',
-  }).then((response) => {
+export const fetchDeleteTask = async (taskId) => {
+  try {
+    const response = await fetch(`${baseUrl}/${taskId}`, {
+      method: 'DELETE',
+    });
     if (!response.ok) {
       throw new Error('Failed to delete task');
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const fetchTasks = () =>
-  fetch(baseUrl).then((resp) => {
+export const fetchTasks = async () => {
+  try {
+    const resp = await fetch(baseUrl);
     if (resp.ok) {
-      return resp.json();
+      const response = await resp.json();
+      return response;
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
+};
